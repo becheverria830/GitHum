62066a2333eb5fd16c8a5dbf528ce4d83d845edf
Britney Echeverria <britney@Britneys-Air.home>
2020-10-28 03:03:58 -0400

    fix friend box css (add scroll, fix action buttons) and add title to container

--- client/src/components/FriendBox/friendBox.jsx
+++ client/src/components/FriendBox/friendBox.jsx
@@ -20,30 +20,46 @@ class FriendBox extends Component {
     return (
       <React.Fragment>
         <Col lg="4" md="4" sm="4" xs="4">
+          <h6 id="friend-box-title">Friends</h6>
           <Container
             className="container container-fluid"
             id="friend-action-container"
           >
-            <Row>
-              <Col lg="12" md="12" sm="12" xs="12">
-                <Table className="friend-list-table">
-                  <tbody>
-                    <tr className="friend-list-item">Somebody</tr>
-                    <tr className="friend-list-item">Somebody2</tr>
-                    <tr className="friend-list-item">Somebody3</tr>
-                    <tr className="friend-list-item">Somebody4</tr>
-                  </tbody>
-                </Table>
-              </Col>
-            </Row>
+            <Container id="friend-list-container">
+              <Row>
+                <Col lg="12" md="12" sm="12" xs="12">
+                  <Table className="friend-list-table">
+                    <tbody>
+                      <tr className="friend-list-item">Somebody</tr>
+                      <tr className="friend-list-item">Somebody2</tr>
+                      <tr className="friend-list-item">Somebody3</tr>
+                      <tr className="friend-list-item">Somebody4</tr>
+                      <tr className="friend-list-item">Somebody</tr>
+                      <tr className="friend-list-item">Somebody2</tr>
+                      <tr className="friend-list-item">Somebody3</tr>
+                      <tr className="friend-list-item">Somebody4</tr>
+                      <tr className="friend-list-item">Somebody</tr>
+                      <tr className="friend-list-item">Somebody2</tr>
+                      <tr className="friend-list-item">Somebody3</tr>
+                      <tr className="friend-list-item">Somebody4</tr>
+                      <tr className="friend-list-item">Somebody</tr>
+                      <tr className="friend-list-item">Somebody2</tr>
+                      <tr className="friend-list-item">Somebody3</tr>
+                      <tr className="friend-list-item">Somebody4</tr>
+                    </tbody>
+                  </Table>
+                </Col>
+              </Row>
+            </Container>
+
             <Row>
               <Col lg="6" md="6" sm="6" xs="6">
-                <Button className="friend-list-action-button">
+                <Button className="friend-list-action-button" id="add-friend-button">
                   Add Friend
                 </Button>
               </Col>
               <Col lg="6" md="6" sm="6" xs="6">
-                <Button className="friend-list-action-button">
+                <Button className="friend-list-action-button" id="friend-requests-button">
                   Friend Requests
                 </Button>
               </Col>
