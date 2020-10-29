30b8ff396c05ec835bcd201a93d10891ea371c72
Britney Echeverria <britney@Britneys-Air.home>
2020-10-29 02:59:49 -0400

    start deforest modal

---
+++ client/src/components/Forest/deforest.jsx
@@ -0,0 +1,74 @@
+/* Importing React & Router */
+import React, { Component, useState } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+
+/* Importing All Bootstrap Components */
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+
+/* Importing All Resources & Custom CSS */
+import "./branchForestForest.css";
+
+function BranchForestButton() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button id="branch-forest-button" onClick={handleShow}>
+        Branch From Forest
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header
+          closeButton
+          id="branch-forest-modal-header"
+          className="text-center"
+        >
+          <Modal.Title id="branch-forest-modal-title">
+            Start a Forest
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body id="branch-forest-modal-body">
+          <Row>
+            <Col lg="12" md="12" sm="12" xs="12">
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Give your Forest a name!"
+                  className="ml-sm-2"
+                  id="branch-forest-search-bar"
+                />
+                <Link to="/forest/1">
+                  <input
+                    id="branch-forest-create-link"
+                    type="submit"
+                    value="Create"
+                  ></input>
+                </Link>
+              </Form>
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default BranchForestButton;
+
+class BranchForest extends Component {
+  state = {};
+  render() {
+    return <BranchForestButton />;
+  }
+}
