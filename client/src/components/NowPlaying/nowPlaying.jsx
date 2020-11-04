640eadb73da0f4848d918080c0af8b89eb90cb70
Jeremy Herrmann <Jeremy.herrmann@stonybrook.edu>
2020-11-04 17:55:56 -0500

    Attempted to get now playing working

--- client/src/components/NowPlaying/nowPlaying.jsx
+++ client/src/components/NowPlaying/nowPlaying.jsx
@@ -22,11 +22,124 @@ class NowPlaying extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      token: "INSERT YOUR TOKEN HERE :)",
       queue: {
         songs: [],
         index: -1,
       },
+      deviceId: "",
+      trackName: "Track Name",
+      artistName: "Artist Name",
+      albumName: "Album Name",
+      playing: false,
+      position: 0,
+      duration: 1
     };
+  }
+
+  // when we receive a new update from the player
+  onStateChanged(state) {
+    console.log("state changed");
+    // // only update if we got a real state
+    // if (state !== null) {
+    //   const {
+    //     current_track: currentTrack,
+    //     position,
+    //     duration,
+    //   } = state.track_window;
+    //   const trackName = currentTrack.name;
+    //   const albumName = currentTrack.album.name;
+    //   const artistName = currentTrack.artists
+    //     .map(artist => artist.name)
+    //     .join(", ");
+    //   const playing = !state.paused;
+    //   this.setState({
+    //     position,
+    //     duration,
+    //     trackName,
+    //     albumName,
+    //     artistName,
+    //     playing
+    //   });
+    // } else {
+    //   // state was null, user might have swapped to another device
+    //   this.setState({ error: "Looks like you might have swapped to another device?" });
+    // }
+  }
+
+  checkForPlayer() {
+    const { token } = this.state;
+    // if the Spotify SDK has loaded
+    if (window.Spotify !== undefined) {
+      // cancel the interval
+      clearInterval(this.playerCheckInterval);
+      // create a new player
+      this.player = new window.Spotify.Player({
+        name: "GitHum",
+        getOAuthToken: cb => { cb(token); },
+      });
+      // problem setting up the player
+      this.player.on('initialization_error', e => { console.error(e); });
+      // problem authenticating the user.
+      // either the token was invalid in the first place,
+      // or it expired (it lasts one hour)
+      this.player.on('authentication_error', e => {
+        console.error(e);
+      });
+      // currently only premium accounts can use the API
+      this.player.on('account_error', e => { console.error(e); });
+      // loading/playing the track failed for some reason
+      this.player.on('playback_error', e => { console.error(e); });
+
+      // Playback status updates
+      this.player.on('player_state_changed', state => this.onStateChanged(state));
+
+      // Ready
+      this.player.on('ready', async data => {
+        let { device_id } = data;
+        console.log("Let the music play on!");
+        // set the deviceId variable, then let's try
+        // to swap music playback to *our* player!
+        await this.setState({ deviceId: device_id });
+        this.transferPlaybackHere();
+      });
+
+      // finally, connect!
+      this.player.connect();
+    }
+  }
+
+  onPrevClick() {
+    console.log("prev");
+    this.player.previousTrack();
+  }
+
+  onPlayClick() {
+    console.log("play");
+    this.player.togglePlay();
+  }
+
+  onNextClick() {
+    console.log("next`");
+    this.player.nextTrack();
+  }
+
+  transferPlaybackHere() {
+    const { deviceId, token } = this.state;
+    // https://beta.developer.spotify.com/documentation/web-api/reference/player/transfer-a-users-playback/
+    fetch("https://api.spotify.com/v1/me/player", {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "device_ids": [ deviceId ],
+        // true: start playing music if it was paused on the other device
+        // false: paused if paused on other device, start playing music otherwise
+        "play": true,
+      }),
+    });
   }
 
   getQueueData() {
@@ -43,6 +156,7 @@ class NowPlaying extends Component {
 
   componentDidMount() {
     this.getQueueData();
+    this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
   }
 
   render() {
@@ -78,13 +192,16 @@ class NowPlaying extends Component {
                 src={ShuffleIcon}/>
               <Image
                 className="now-playing-icon"
-                src={FastRewindIcon}/>
+                src={FastRewindIcon}
+                onClick={() => this.onPrevClick()}/>
               <Image
                 className="now-playing-icon"
-                src={PlayCircleIcon}/>
+                src={PlayCircleIcon}
+                onClick={() => this.onPlayClick()}/>
               <Image
                 className="now-playing-icon"
-                src={FastForwardIcon}/>
+                src={FastForwardIcon}
+                onClick={() => this.onNextClick()}/>
               <Image
                 className="now-playing-icon"
                 src={LoopIcon}/>
