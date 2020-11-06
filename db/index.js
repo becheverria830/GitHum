2dfaf7ad4fa7ea159dfa6744a8842e2b2ef6a498
Isabelle Greenberg <isabellegreenberg@gmail.com>
2020-11-06 00:13:16 -0500

    Revert "small tutorial changes"
    This reverts commit 50e37eaba626240c27c3117e08144c2abf7deb39.

--- db/index.js
+++ db/index.js
@@ -6,7 +6,6 @@ require('./config');
 const typeDefs = gql`
     type Query {
         hello: String
-        
     }
 `;
 
@@ -21,5 +20,5 @@ const app = express();
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
