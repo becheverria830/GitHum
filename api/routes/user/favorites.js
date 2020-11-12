e3dcd80449319fa19f9bf9c191b01e38b706f8b5
Isabelle Greenberg <isabellegreenberg@gmail.com>
2020-11-12 01:25:04 -0500

    Favorites Pseudocode + Gameplan

--- api/routes/user/favorites.js
+++ api/routes/user/favorites.js
@@ -5,7 +5,27 @@ var router = express.Router();
 /user/favorites/songs
 */
 
+
+// Turn a song into a favorite
+router.post("/addsong", function (req, res, next) {
+  // check that the string for the song info is valid
+
+  // if not then return with e
+  
+  // Check if song has an object already
+
+  // If So, then just add that _id to user.library.favorites
+
+  // If Not, then create the object so we can populate the list,
+  // and as the object is being made, save the ID for it
+
+
+});
+
+
+//Populate the favorite songs
 router.get('/songs', function(req, res, next) {
+ // replace with db request
   res.json({
     "songs": [
       {
