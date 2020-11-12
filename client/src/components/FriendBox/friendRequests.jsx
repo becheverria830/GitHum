21f2276dbc22f4d0db021d92726aa7726deb3f20
Britney Echeverria <britney@Britneys-Air.home>
2020-11-12 01:51:17 -0500

    incoming an doutgoing requests show correctly on toggle

--- client/src/components/FriendBox/friendRequests.jsx
+++ client/src/components/FriendBox/friendRequests.jsx
@@ -1,6 +1,6 @@
 /* Importing React & Router */
 import React, { Component, useState } from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, withRouter } from "react-router-dom";
 
 /* Importing All Bootstrap Components */
 import Container from "react-bootstrap/Container";
@@ -13,16 +13,38 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
 /* Importing All Resources & Custom CSS */
 import "./friendRequests.css";
 import MainNavBar from "../MainNavBar/mainNavBar";
 import NowPlaying from "../NowPlaying/nowPlaying";
 import SearchIcon from "../../assets/search.svg";
 
-class FriendRequestsButton extends Component {
-  state = {
-    show: false,
-  };
+class FriendRequests extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      showIncoming: true,
+      showOutgoing: false,
+      selectedRequest: [],
+      friends: {
+        current_friends: [],
+        requests: {
+          received: [],
+          sent: [],
+        },
+      },
+    };
+    this.displayIncoming = this.displayIncoming.bind(this);
+    this.displayOutgoing = this.displayOutgoing.bind(this);
+    this.handleSelectedRequest = this.handleSelectedRequest.bind(this);
+    this.handleAcceptRequest = this.handleAcceptRequest.bind(this);
+    this.handleDeclineRequest = this.handleDeclineRequest.bind(this);
+  }
+
   showModal = (e) => {
     this.setState({
       show: !this.state.show,
@@ -32,13 +54,46 @@ class FriendRequestsButton extends Component {
     this.props.onClose && this.props.onClose(e);
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      // userUsername: "",
-      // otherUsername: "",
-    };
+  displayIncoming() {
+    this.setState({
+      showIncoming: true,
+      showOutgoing: false,
+    });
   }
+
+  displayOutgoing() {
+    this.setState({
+      showIncoming: false,
+      showOutgoing: true,
+    });
+  }
+
+  getFriends() {
+    fetch("http://localhost:9000/user/friends/" + this.props.auth.user.id)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          friends: {
+            current_friends: res.list,
+            requests: {
+              received: res.incoming_requests,
+              sent: res.outgoing_requests,
+            },
+          },
+        });
+        console.log(res);
+      })
+      .catch((err) => err);
+    console.log(this.state.friends.requests.sent);
+  }
+
+  handleSelectedRequest(event, request) {
+    this.setState({ selectedRequest: request._id });
+  }
+
+  handleAcceptRequest(event) {}
+
+  handleDeclineRequest(event) {}
 
   render() {
     return (
@@ -47,6 +102,7 @@ class FriendRequestsButton extends Component {
           className="friend-button"
           onClick={(e) => {
             this.showModal();
+            this.getFriends();
           }}
         >
           Friend Requests
@@ -71,7 +127,10 @@ class FriendRequestsButton extends Component {
                 xs="6"
                 className="friend-requests-toggle-button-column"
               >
-                <Button className="friend-requests-toggle-buttons">
+                <Button
+                  className="friend-requests-toggle-buttons"
+                  onClick={this.displayIncoming}
+                >
                   Incoming Requests
                 </Button>
               </Col>
@@ -82,7 +141,10 @@ class FriendRequestsButton extends Component {
                 xs="6"
                 className="friend-requests-toggle-button-column"
               >
-                <Button className="friend-requests-toggle-buttons">
+                <Button
+                  className="friend-requests-toggle-buttons"
+                  onClick={this.displayOutgoing}
+                >
                   Outgoing Requests
                 </Button>
               </Col>
@@ -90,28 +152,50 @@ class FriendRequestsButton extends Component {
             <Row>
               <Container className="friend-requests-container">
                 <Row className="friend-requests-items">
-                  <Table className="friend-requests-results">
-                    <tbody>
-                      <tr className="friend-requests-item">
-                        <td> PrincessDaisy </td>
-                      </tr>
-                      <tr className="friend-requests-item">
-                        <td> PrincessPeach </td>
-                      </tr>
-                      <tr className="friend-requests-item">
-                        <td> Mario_Br0 </td>
-                      </tr>
-                      <tr className="friend-requests-item">
-                        <td> Gooooonbas </td>
-                      </tr>
-                      <tr className="friend-requests-item">
-                        <td> Toadette123 </td>
-                      </tr>
-                    </tbody>
-                  </Table>
+                  <div className={this.state.showOutgoing ? null : "hidden"}>
+                    <Table className="friend-requests-results">
+                      <tbody>
+                        {this.state.friends.requests.sent.map((outgoingRes) => (
+                          <tr className="friend-requests-item">
+                            <td
+                              value={outgoingRes.username}
+                              onClick={(event) => {
+                                this.handleSelectedRequest(event, outgoingRes);
+                              }}
+                            >
+                              {outgoingRes.username}
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </div>
+                  <div className={this.state.showIncoming ? null : "hidden"}>
+                    <Table className="friend-requests-results">
+                      <tbody>
+                        {this.state.friends.requests.received.map(
+                          (incomingRes) => (
+                            <tr className="friend-requests-item">
+                              <td
+                                value={incomingRes.username}
+                                onClick={(event) => {
+                                  this.handleSelectedRequest(
+                                    event,
+                                    incomingRes
+                                  );
+                                }}
+                              >
+                                {incomingRes.username}
+                              </td>
+                            </tr>
+                          )
+                        )}
+                      </tbody>
+                    </Table>
+                  </div>
                 </Row>
 
-                <Row>
+                <Row className={this.state.showIncoming ? null : "hidden"}>
                   <Col
                     lg="6"
                     md="6"
@@ -140,4 +224,10 @@ class FriendRequestsButton extends Component {
   }
 }
 
-export default FriendRequestsButton;
+FriendRequests.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps)(withRouter(FriendRequests));
