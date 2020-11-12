15ee6c16f4e99599fb5b8ab1f9bb05377196d225
Jeremy Herrmann <Jeremy.herrmann@stonybrook.edu>
2020-11-12 17:51:56 -0500

    fix visit forest after creating it

--- client/src/components/Valley/startForest.jsx
+++ client/src/components/Valley/startForest.jsx
@@ -68,8 +68,7 @@ class StartForest extends Component {
         if(res.forest == null) {
           alert("Please complete the form and try again!");
         } else {
-          //console.log(res.json());
-          this.props.history.push("/forests/" + res.forest._id);
+          this.props.history.push("/forest/" + res.forest._id);
         }
       })
       .catch((err) => err);
