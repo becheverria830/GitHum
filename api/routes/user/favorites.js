e5d17e9916672c4833a5293f35407173988a41db
Britney Echeverria <britney@Britneys-Air.home>
2020-11-12 20:35:48 -0500

    fixed some js stuffs for favorites and forests

--- api/routes/user/favorites.js
+++ api/routes/user/favorites.js
@@ -1,14 +1,13 @@
-var express = require('express');
-const models  = require("../models");
+var express = require("express");
+const models = require("../models");
 
 var router = express.Router();
 
-const User = models['user'];
+const User = models["user"];
 
 /*
 /user/favorites/songs
 */
-
 
 // Turn a song into a favorite
 router.post("/songs/add", function (req, res, next) {
@@ -17,31 +16,26 @@ router.post("/songs/add", function (req, res, next) {
   const songlink = req.body.songlink;
 
   // If not then return with e
-  if(songlink == undefined || userid == undefined){
+  if (songlink == undefined || userid == undefined) {
+    console.log(songlink);
+    console.log(userid);
     res.status(400).json({
-      "user": null
+      user: null,
     });
   } else {
-    res.status(400).json({
-      "user": null
-    });
+    res.status(200).json({});
   }
-    // Check if song has an object already
-    
+  // Check if song has an object already
 
   // If So, then just add that _id to user.library.favorites
 
   // If Not, then create the object so we can populate the list,
   // and as the object is being made, save the ID for it
-
-
 });
 
-
 //Populate the favorite songs
-router.get('/songs', function(req, res, next) {
+router.get("/songs", function (req, res, next) {
   // replace with db request
-  
 });
 
 module.exports = router;
--- api/routes/user/forests.js
+++ api/routes/user/forests.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 
-const models  = require("../models");
+const models = require("../models");
 
 var router = express.Router();
 
-const Forest = models['forests'];
-const User = models['user'];
+const Forest = models["forests"];
+const User = models["user"];
 
 /*
 /user/forests?userid=
@@ -19,260 +19,270 @@ router.post("/create", (req, res, err) => {
   console.log(name);
   console.log(userid);
 
-  if(name == undefined || userid == undefined){
+  if (name == undefined || userid == undefined) {
     res.status(400).json({
-      "forest": null
+      forest: null,
     });
   } else {
     var forest_data = {
       name: name,
       icon: "",
       active: true,
-      children:[],
+      children: [],
       depth: 1,
       creator: userid,
       songs: [],
       settings: {
-        privacy: false
-      }
-    }
-    Forest.insertMany([forest_data], function(create_error, result) {
+        privacy: false,
+      },
+    };
+    Forest.insertMany([forest_data], function (create_error, result) {
       if (create_error) throw create_error;
       console.log(result);
       res.status(200).json({
-        "forest": result[0]
+        forest: result[0],
       });
     });
   }
 });
 
-router.get('/forests/:userid', function(req, res, next) {
+router.get("/forests/:userid", function (req, res, next) {
   console.log("inside here");
   //Getting the information from the request
   const userid = req.params.userid;
 
-  if(userid == undefined){
+  if (userid == undefined) {
     //Throwing an exception if user didn't supply all the information.
     res.status(400).json({
-      "forests":[]
+      forests: [],
     });
   } else {
     //Finding if a user with the email exists already
-    Forest.find({ 'creator': userid }, function (find_error, forests) {
+    Forest.find({ creator: userid }, function (find_error, forests) {
       if (find_error) throw find_error;
 
-      refined_forests = []
-      for(forest of forests) {
+      refined_forests = [];
+      for (forest of forests) {
         refined_forests.push(forest);
       }
       res.status(200).json({
-        "forests": refined_forests
+        forests: refined_forests,
       });
-    })
+    });
   }
 });
 
-
-router.get('/forests/:forestid', function(req, res, next) {
-  
+router.get("/:forestid", function (req, res, next) {
   var id = req.params.forestid;
-  
+
   console.log("inside AAAAAA here");
   //Getting the information from the request
-  const userid = req.params.userid;
+  // const userid = req.params.userid;
 
-  if(userid == undefined){
+  if (id == undefined) {
     //Throwing an exception if user didn't supply all the information.
     res.status(400).json({
-      "forests":[]
+      forests: [],
     });
   } else {
     //Finding if a user with the email exists already
-    Forest.find({'_id': id }, function (find_error, forests) {
+    Forest.find({ _id: id }, function (find_error, forests) {
       if (find_error) throw find_error;
 
-      refined_forests = []
-      for(forest of forests) {
+      refined_forests = [];
+      for (forest of forests) {
         refined_forests.push(forest);
         console.log("WHERE AM IIIIIIIIIIIIIIIIIIIIIII");
       }
       res.status(200).json({
-        "forests": refined_forests
+        forests: refined_forests,
       });
-    })
+    });
   }
-
-
-
-
-
-
-
-
-router.get('/saved', function(req, res, next) {
+});
+router.get("/saved", function (req, res, next) {
   res.json({
-    "forests": [
+    forests: [
       {
-        "id": 1,
-        "name": "My First Forest",
-        "icon": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
-        "active": 1,
-        "songs": [
+        id: 1,
+        name: "My First Forest",
+        icon:
+          "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+        active: 1,
+        songs: [
           {
-            "id": 1,
-            "song_name": "Piano Man",
-            "artist_name": "Billy Joel",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 1,
+            song_name: "Piano Man",
+            artist_name: "Billy Joel",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 2,
-            "song_name": "Up Town Funk",
-            "artist_name": "Bruno Mars",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
-          }
+            id: 2,
+            song_name: "Up Town Funk",
+            artist_name: "Bruno Mars",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+          },
         ],
-        "settings": {
-          "privacy": 1
-        }
+        settings: {
+          privacy: 1,
+        },
       },
       {
-        "id": 2,
-        "name": "My Second Forest",
-        "icon": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
-        "active": 1,
-        "songs": [
+        id: 2,
+        name: "My Second Forest",
+        icon:
+          "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+        active: 1,
+        songs: [
           {
-            "id": 1,
-            "song_name": "Piano Man",
-            "artist_name": "Billy Joel",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 1,
+            song_name: "Piano Man",
+            artist_name: "Billy Joel",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 2,
-            "song_name": "Up Town Funk",
-            "artist_name": "Bruno Mars",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
-          }
+            id: 2,
+            song_name: "Up Town Funk",
+            artist_name: "Bruno Mars",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+          },
         ],
-        "settings": {
-          "privacy": 1
-        }
+        settings: {
+          privacy: 1,
+        },
       },
       {
-        "id": 3,
-        "name": "My Third Forest",
-        "icon": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
-        "active": 1,
-        "songs": [
+        id: 3,
+        name: "My Third Forest",
+        icon:
+          "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+        active: 1,
+        songs: [
           {
-            "id": 1,
-            "song_name": "Piano Man",
-            "artist_name": "Billy Joel",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 1,
+            song_name: "Piano Man",
+            artist_name: "Billy Joel",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 2,
-            "song_name": "Up Town Funk",
-            "artist_name": "Bruno Mars",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
-          }
+            id: 2,
+            song_name: "Up Town Funk",
+            artist_name: "Bruno Mars",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+          },
         ],
-        "settings": {
-          "privacy": 1
-        }
+        settings: {
+          privacy: 1,
+        },
       },
-    ]
+    ],
   });
 });
 
-router.get('/friends', function(req, res, next) {
+router.get("/friends", function (req, res, next) {
   res.json({
-    "forests": [
+    forests: [
       {
-        "id": 1,
-        "name": "My First Forest",
-        "icon": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
-        "active": 1,
-        "songs": [
+        id: 1,
+        name: "My First Forest",
+        icon:
+          "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+        active: 1,
+        songs: [
           {
-            "id": 1,
-            "song_name": "Piano Man",
-            "artist_name": "Billy Joel",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 1,
+            song_name: "Piano Man",
+            artist_name: "Billy Joel",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 2,
-            "song_name": "Up Town Funk",
-            "artist_name": "Bruno Mars",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 2,
+            song_name: "Up Town Funk",
+            artist_name: "Bruno Mars",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 3,
-            "song_name": "San Francisco",
-            "artist_name": "The Mowgli's",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
-          }
+            id: 3,
+            song_name: "San Francisco",
+            artist_name: "The Mowgli's",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+          },
         ],
-        "settings": {
-          "privacy": 1
-        }
+        settings: {
+          privacy: 1,
+        },
       },
       {
-        "id": 2,
-        "name": "My Second Forest",
-        "icon": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
-        "active": 1,
-        "songs": [
+        id: 2,
+        name: "My Second Forest",
+        icon:
+          "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+        active: 1,
+        songs: [
           {
-            "id": 1,
-            "song_name": "Piano Man",
-            "artist_name": "Billy Joel",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 1,
+            song_name: "Piano Man",
+            artist_name: "Billy Joel",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 2,
-            "song_name": "Up Town Funk",
-            "artist_name": "Bruno Mars",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
-          }
+            id: 2,
+            song_name: "Up Town Funk",
+            artist_name: "Bruno Mars",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+          },
         ],
-        "settings": {
-          "privacy": 1
-        }
+        settings: {
+          privacy: 1,
+        },
       },
       {
-        "id": 3,
-        "name": "My Third Forest",
-        "icon": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
-        "active": 1,
-        "songs": [
+        id: 3,
+        name: "My Third Forest",
+        icon:
+          "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+        active: 1,
+        songs: [
           {
-            "id": 1,
-            "song_name": "Piano Man",
-            "artist_name": "Billy Joel",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 1,
+            song_name: "Piano Man",
+            artist_name: "Billy Joel",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 2,
-            "song_name": "Up Town Funk",
-            "artist_name": "Bruno Mars",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
+            id: 2,
+            song_name: "Up Town Funk",
+            artist_name: "Bruno Mars",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
           },
           {
-            "id": 3,
-            "song_name": "San Francisco",
-            "artist_name": "The Mowgli's",
-            "album_art": "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7"
-          }
+            id: 3,
+            song_name: "San Francisco",
+            artist_name: "The Mowgli's",
+            album_art:
+              "https://i.scdn.co/image/ab67616d00001e02814cbc4746358a25c84c62e7",
+          },
         ],
-        "settings": {
-          "privacy": 1
-        }
+        settings: {
+          privacy: 1,
+        },
       },
-    ]
+    ],
   });
 });
-
 
 /*
   res.json({
@@ -299,6 +309,5 @@ router.get('/friends', function(req, res, next) {
     }
   });
   */
-});
 
 module.exports = router;
