4633006d97cc3c2e161fee4b950a1b664f19e116
Jeremy Herrmann <Jeremy.herrmann@stonybrook.edu>
2020-12-04 18:00:00 -0500

    updating keys

--- api/config/keys.js
+++ api/config/keys.js
@@ -11,7 +11,7 @@ const Twilio_Key =
   "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";
 const Spotify_Client_ID = "f052d98613e44dd1bcff5da4fa1b2c0a";
 const Spotify_Client_Secret = "3f079d1cede8444a88627e4dfa2ab161";
-const Spotify_OAuth_Token = "BQC8r6-hGncIUTGCm38yU_o6spnnm0zeNilhfuVakxE0T6g4OqFbWSgRiuDtwts1-G2Z1Og8kfq9a66o12y1eUUCHVaoVN14_TIEKYNIl5lAnr4SluQgqv5tl9HfqS1KhIbp1lBDjc-i18c";
+const Spotify_OAuth_Token = "BQBtCMnn5uL_pvU5HfbCrLPiEVCYMpOVe2tnKW2ca7qdjg5ce1uZb4nMruMz8vitBrlrlvJDYfLSrvUmepwAMbkikKYn7Fwlj1T0yFo-Hp8otYUSU4gQRFu2W0g13rxkJreYHhhZmwZOPFk";
 
 module.exports = {
   MongoDB_URI: MongoDB_URI,
