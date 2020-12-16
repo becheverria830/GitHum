39be85f920ad965d2ea35c99e5e786c5976959e8
Britney Echeverria <britney@Britneys-Air.home>
2020-12-16 04:57:19 -0500

    auto refresh search token

--- api/routes/search.js
+++ api/routes/search.js
@@ -12,7 +12,24 @@ var spotifyApi = new SpotifyWebApi({
   clientId: keys.Spotify_Client_ID,
   clientSecret: keys.Spotify_Client_Secret
 });
-spotifyApi.setAccessToken(keys.Spotify_OAuth_Token);
+
+function intervalFunc() {
+  // Retrieve an access token.
+  spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+      console.log('The access token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function(err) {
+      console.log('Something went wrong when retrieving an access token', err);
+    }
+  );
+}
+  setInterval(intervalFunc, 2700000);
+  intervalFunc();
 
 /*
 /search?query=..
